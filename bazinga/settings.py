42cc9f3ddcbc67c0be3896ff98290f54096d13a1
elprincipe47 <elhassanegargem@gmail.com>
2019-09-01 12:59:27 +0100

    dj database formatting

--- bazinga/settings.py
+++ bazinga/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,20 +80,22 @@ WSGI_APPLICATION = 'bazinga.wsgi.application'
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'bazinga',
+#         'USER': 'bazingauser',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bazinga',
-        'USER': 'bazingauser',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-    }
-}
 
+# SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
